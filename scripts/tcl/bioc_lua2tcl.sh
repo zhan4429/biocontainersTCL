cb2d3be61f869500e6ea1b59471500df04633a5c
Yucheng Zhang <yzhang85@login-prod-01.pax.tufts.edu>
2023-12-18 15:47:00 -0500

    the script is ready now

--- scripts/tcl/bioc_lua2tcl.sh
+++ scripts/tcl/bioc_lua2tcl.sh
@@ -2,27 +2,46 @@
 
 #  bioc_lua2tcl.sh - Covert biocontainers modulefiles in lua format developed by Purdue University to tcl format.
 #  The script will also create bash wrapper each exectuable provided by the application.
-#  Usage: bash bioc_lua2tcl.sh - no parameter is required. Need to check directories are correct. 
+#  Usage: bash bioc_lua2tcl.sh -i Lua_dir -o tcl_output. 
 #
 #
 # By Yucheng Zhang, Tufts University Research Technology <yzhang85@tufts.edu>, 2023
 
+# Define options and default values
+input_dir=""
+output_dir=""
+
+# Define functions
+# ----------------------------------------------------------------------
+# Function to display usage information
+# ----------------------------------------------------------------------
+print_usage() {
+  echo "Usage: $0 [--help/-h] [--input/-i INPUT_DIR] [--output/-o OUTPUT_DIR]"
+  echo "  -h, --help     Display help message and exit."
+  echo "  -i, --input    Path to the input directory containing lua files."
+  echo "  -o, --output  Path to the output directory (default: tcls)."
+  exit 1
+}
+
+# ----------------------------------------------------------------------
+# Function generate_new_modulefile definition
+# ----------------------------------------------------------------------
 
 generate_new_modulefile() {
-# generate_new_modulefile "VERSION"
-# This is used to convert lua files developed by Purdue University to TCL modulefiles
-local app="$1"
-local version="$2"
-local lua="$3"
-local tcl="$4"
-local conflict_list=$(grep ^conflict $lua | grep -v myModule | cut -d '(' -f 2 | sed 's/)//g' | sed 's/ //g' | sed 's/\"//g')
-local conflict_list=$conflict_list",$app"
-IFS=',' read -r -a conflict_array <<< "$conflict_list"
-local conflict_uniq_array=( $(printf "%s\n" ${conflict_array[@]} | sort -u) )
-local conflicts=$(printf  "%s " "${conflict_uniq_array[@]}")
-local conflicts=$(echo $conflicts | xargs echo -n)
+    # generate_new_modulefile $APP $VERSION $LUA $TCLout
+    # This is used to convert lua files developed by Purdue University to TCL modulefiles
+    local app="$1"
+    local version="$2"
+    local lua="$3"
+    local tcl="$4"
+    local conflict_list=$(grep ^conflict $lua | grep -v myModule | cut -d '(' -f 2 | sed 's/)//g' | sed 's/ //g' | sed 's/\"//g')
+    local conflict_list=$conflict_list",$app"
+    IFS=',' read -r -a conflict_array <<< "$conflict_list"
+    local conflict_uniq_array=( $(printf "%s\n" ${conflict_array[@]} | sort -u) )
+    local conflicts=$(printf  "%s " "${conflict_uniq_array[@]}")
+    local conflicts=$(echo $conflicts | xargs echo -n)
 
-cat <<EOF >>$tcl
+    cat <<EOF >>$tcl
 #%Module -*- tcl -*-
 # The MIT License (MIT)
 # Copyright (c) 2023 Tufts University
@@ -46,47 +65,46 @@ cat <<EOF >>$tcl
 # IN THE SOFTWARE.
 
 EOF
-description=$(grep whatis $lua | grep "Description" | cut -d \" -f2)
-homepage=$(grep whatis $lua | grep "Home" | cut -d \" -f2)
-biocontainers=$(grep whatis $lua |grep "BioContainers" | cut -d \" -f2)
-dockerhub=$(grep whatis $lua | grep "Docker"  | cut -d \" -f2)
-additional_bind=($(grep SINGULARITY_BIND $lua | cut -d \" -f4))
-variables=$(grep pushenv $lua |sed 's/pushenv/setenv/g' | sed 's/(/ /g' |  sed 's/)/ /g' | sed 's/,/ /g')
-echo "module-whatis   \"$description\"" >> $tcl
-echo "module-whatis   \"$homepage\"" >> $tcl
-echo "module-whatis \"Commands: $programs\"" >> $tcl
+    description=$(grep whatis $lua | grep "Description" | cut -d \" -f2)
+    homepage=$(grep whatis $lua | grep "Home" | cut -d \" -f2)
+    biocontainers=$(grep whatis $lua |grep "BioContainers" | cut -d \" -f2)
+    dockerhub=$(grep whatis $lua | grep "Docker"  | cut -d \" -f2)
+    additional_bind=($(grep SINGULARITY_BIND $lua | cut -d \" -f4))
+    variables=$(grep pushenv $lua |sed 's/pushenv/setenv/g' | sed 's/(/ /g' |  sed 's/)/ /g' | sed 's/,/ /g')
+    echo "module-whatis   \"$description\"" >> $tcl
+    echo "module-whatis   \"$homepage\"" >> $tcl
+    echo "module-whatis \"Commands: $PROGRAMS\"" >> $tcl
 
-if [ ! -z "$biocontainers" ]
-then
-	echo "module-whatis   \"$biocontainers\"" >> $tcl
-fi
-	
-if [ ! -z "$dockerhub" ]
-then
-	echo "module-whatis   \"$dockerhub\"" >> $tcl
-fi
+    if [ ! -z "$biocontainers" ]
+    then
+      echo "module-whatis   \"$biocontainers\"" >> $tcl
+    fi
+      
+    if [ ! -z "$dockerhub" ]
+    then
+      echo "module-whatis   \"$dockerhub\"" >> $tcl
+    fi
 
-
-cat <<EOF >>$tcl
+cat <<EOF >>$tcl    
 
 set pkg $app 
-set ver $ver
+set ver $version
 
 proc ModulesHelp { } {
-  puts stderr "\tThis module adds $app v$ver to the environment.  It runs as a container under singularity"
+  puts stderr "\tThis module adds $app v$version to the environment.  It runs as a container under singularity"
 }
 
 #
 # prepend-path and set SINGULARITY_BIND
 #
-prepend-path PATH            /cluster/tufts/biocontainers/tools/$app/$ver/bin
+prepend-path PATH            $AppTool_dir
 prepend-path --delim=, SINGULARITY_BIND /cluster 
 EOF
 
 
-if (( ${#additional_bind[@]} )); then
-    printf 'prepend-path --delim=, SINGULARITY_BIND  %s\n' "${additional_bind[@]}"  >> $tcl
-fi
+      if (( ${#additional_bind[@]} )); then
+          printf 'prepend-path --delim=, SINGULARITY_BIND  %s\n' "${additional_bind[@]}"  >> $tcl
+      fi
 
 cat <<EOF >>$tcl
 
@@ -122,7 +140,7 @@ if {[module-info mode "load"]} {
   system [concat "logger environment-modules" [module-info name] \$the_user ]
 }
 
-set additional_prereqs {"singularity/3.8.4" "squashfs/4.4"}
+set additional_prereqs {"singularity"}
 if {[module-info mode "load"]} {
   foreach a_module \$additional_prereqs {
     if {![is-loaded \$a_module]} {
@@ -135,20 +153,26 @@ EOF
 }
 
 
+# ----------------------------------------------------------------------
+# Function generate_executable definition
+# ----------------------------------------------------------------------
+
 generate_executable() {
-# This is used to convert bash wrappers for commands provided by applications.
-local app=$1
-local version=$2
-local command=$3
-local executable=/cluster/tufts/rt/yzhang85/biocontainers/tools/$app/$ver/bin/$command
-local lua=$4
-local IMAGE=$(grep "local image" $lua | cut -d \" -f 2)        
-local ENTRYPOINT_ARGS=$(grep 'local entrypoint_args' $lua | cut -d \" -f 2)
+      # This is used to convert bash wrappers for commands provided by applications.
+      # generate_executable $APP $VERSION $PROGRAM $LUA
+      local app=$1
+      local version=$2
+      local command=$3
+      local executable=$AppTool_dir/$command
+      local lua=$4
+      local IMAGE=$(grep "local image" $lua | cut -d \" -f 2)        
+      local ENTRYPOINT_ARGS=$(grep 'local entrypoint_args' $lua | cut -d \" -f 2)
+echo $executable
 cat <<EOF >>$executable
 #!/usr/bin/env bash
 
 if [ ! \$(command -v singularity) ]; then
-        module load singularity/3.8.4 squashfs/4.4
+        module load singularity
 fi
 
 VER=$version
@@ -161,45 +185,90 @@ ENTRYPOINT_ARGS="$ENTRYPOINT_ARGS"
 ## Determine Nvidia GPUs (to pass coresponding flag to Singularity)
 if [[ \$(nvidia-smi -L 2>/dev/null) ]]
 then
-        echo "BIOC: Enabling Nvidia GPU support in the container."
         OPTIONS="--nv"
 fi
 	
 singularity exec \$OPTIONS \$DIRECTORY/\$IMAGE \$ENTRYPOINT_ARGS \$PROGRAM "\$@"
 EOF
 
-chmod +x $executable
+      chmod +x $executable
 }
 
+# ----------------------------------------------------------------------
+# Main logic
+# ----------------------------------------------------------------------
+# Parse options
+while getopts ":hi:o:" opt; do
+  case $opt in
+    h)
+      print_usage
+      ;;
+    i)
+      input_dir="$OPTARG"
+      ;;
+    o)
+      output_dir="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      print_usage
+      ;;
+  esac
+done
 
-current_dir="$PWD" # save current directory 
-cd ../../ # go up two directories
-repo_path="$PWD" # assign path to repo_path
-lua_dir="$PWD/luas"
-tcl_dir="$PWD/tcls"
-cd $current_dir # cd back to current directory
+# Check for required options and validate input
+if [[ -z "$input_dir" ]]; then
+  echo "Error: Missing the input directory containing lua files" >&2
+  print_usage
+fi
 
-for module in $lua_dir/*; do
-        app="$(basename $module)";
-        version_folder="$module/"
-        filenamesarray=`ls $version_folder*.lua`
-        tcl_folder="$tcl_dir/$app"
-	rm -rf $tcl_folder
-        mkdir -p $tcl_folder
-        for lua in $filenamesarray
+TCLmodules_dir=$output_dir/tcls
+TCLtools_dir=$output_dir/tools
+
+## Create output folders if not exsit
+if [[ ! -d "$TCLmodules_dir" ]]; then
+  mkdir -p "$TCLmodules_dir"
+  echo "Old folder '$TCLmodules_dir' created successfully."
+else
+  echo "Old folder '$TCLmodules_dir' already exists."
+fi
+
+if [[ ! -d "$TCLtools_dir" ]]; then
+  mkdir -p "$TCLtools_dir"
+  echo "Old folder '$TCLtools_dir' created successfully."
+else
+  echo "Old folder '$TCLtools_dir' already exists."
+fi
+
+for APP in $input_dir/*; do
+        APP="$(basename $APP)";
+        VersionArray=$(ls $input_dir/$APP/*.lua)
+        TCL_dir="$TCLmodules_dir/$APP"
+        # Check if the folder exists
+        if [[ -d "$TCL_dir" ]]; then
+            rm -rf "$TCL_dir"
+            echo "Old folder '$TCL_dir' removed successfully."
+        fi
+        mkdir -p $TCL_dir
+        for LUA in $VersionArray
         do
-                ver=$(basename -- "$lua" .lua)
-                tcl_output="$tcl_dir/$app/$ver"
-#               programs=$(grep 'local programs' $lua | cut -d '{' -f 2 |sed 's/}//g'| sed 's/\"//g')
-                programs=$(sed -n '/local programs/,/}/p; /}/q' $lua | tr -d "[:space:]"   | cut -d '{' -f 2 |sed 's/}//g' | sed 's/\"//g' |sed 's/\s*$//g' |
- sed 's/,$//')
-                generate_new_modulefile $app $ver $lua $tcl_output
-                IFS=',' read -r -a program_array <<< "$programs"
-                rm -rf /cluster/tufts/rt/yzhang85/biocontainers/tools/$app/$ver/bin
-                mkdir -p /cluster/tufts/rt/yzhang85/biocontainers/tools/$app/$ver/bin
-                for program in "${program_array[@]}"
-                do
-                        generate_executable $app $ver $program $lua
-                done
+            VERSION=$(basename -- "$LUA" .lua)
+            TCLout="$TCL_dir/$VERSION"
+            PROGRAMS=$(sed -n '/local programs/,/}/p; /}/q' $LUA | tr -d "[:space:]"   | cut -d '{' -f 2 |sed 's/}//g' | sed 's/\"//g' |sed 's/\s*$//g' | sed 's/,$//')
+            ## Generate modulefiles
+            generate_new_modulefile $APP $VERSION $LUA $TCLout
+            ## Generate bash wrappers for exectuables
+            IFS=',' read -r -a PROGRAMarray <<< "$PROGRAMS"
+            # Check if the folder exists
+            AppTool_dir=$TCLtools_dir/$APP/$VERSION/bin
+            if [[ -d "$AppTool_dir" ]]; then
+                rm -rf "$AppTool_dir"
+                echo "Old folder '$AppTool_dir' removed successfully."
+            fi
+            mkdir -p $AppTool_dir
+            for PROGRAM in "${PROGRAMarray[@]}"
+            do
+                  generate_executable $APP $VERSION $PROGRAM $LUA
+            done
         done
 done
